d567cb10c719ae4d9262b8f65a5b9bdb115854df
unknown <deoadel13@gmail.com>
2026-04-20 22:53:22 +0800

    Integrate GetUserQuestionService into Lambda handler

--- lambda-backend/index.mjs
+++ lambda-backend/index.mjs
@@ -1,9 +1,18 @@
-import { fetchS3Context }       from './src/service/FetchS3Context.mjs';
+import { getUserQuestion }      from './src/service/GetUserQuestionService.mjs';
+
 import { extractKeywords }      from './src/service/ExtractKeywordsService.mjs';
+import { fetchS3Context }       from './src/service/FetchS3Context.mjs';
+
+
 
 
 export const handler = async (event) => {
+    // 1. Get User Question
+        const {userQuestion, chatHistory} = await getUserQuestion(event);
+        console.log("User Question:", userQuestion);
+    
 
+    
     // 3.Extract Keywords
     const keywords = await extractKeywords(searchTarget);
     console.log("Extracted Keywords:", keywords);
