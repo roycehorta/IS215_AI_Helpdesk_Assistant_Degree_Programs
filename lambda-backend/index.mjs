255752bb602a2f31af8ba488bdc08e8e931998e5
John Rainer Joaquin <jrjoaquin01@gmail.com>
2026-04-20 20:36:54 +0800

    Integrate ExtractKeywordsService into Lambda handler

--- lambda-backend/index.mjs
+++ lambda-backend/index.mjs
@@ -1,6 +1,13 @@
 import { fetchS3Context }       from './src/service/FetchS3Context.mjs';
+import { extractKeywords }      from './src/service/ExtractKeywordsService.mjs';
+
 
 export const handler = async (event) => {
+
+    // 3.Extract Keywords
+    const keywords = await extractKeywords(searchTarget);
+    console.log("Extracted Keywords:", keywords);
+
 
     // 4. Fetch S3 Context
     const s3Context = await fetchS3Context(keywords);
